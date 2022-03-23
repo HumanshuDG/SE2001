325220c3e369fafa0d17ac22509ac5230a8440f9
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2022-03-23 17:54:21 +0530

    Update PPAs.sh

--- 04 - Week 4/PPAs.sh
+++ 04 - Week 4/PPAs.sh
@@ -1,1 +1,7 @@
+# PPA 1
+ls -l | cut -c 8- | grep rwx | cut -d ' ' -f 12
 
+# PPA 2
+cir=`cat Pincode_info.csv | grep $pin | cut -d ',' -f 1 | cut -d ' ' -f 1`;
+div=`cat Pincode_info.csv | grep $pin | cut -d ',' -f 4 | cut -d ' ' -f 1`;
+echo $cir $div;
