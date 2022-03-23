60b23f885385b6f292e4d7c8491eb2e382ce7044
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2022-03-23 18:02:03 +0530

    Update PPAs.sh

--- 06 - Week 5/PPAs.sh
+++ 06 - Week 5/PPAs.sh
@@ -1,1 +1,19 @@
+# PPA 1
+out="";
+i=1;
+for argument in "$@"; do
+	if (( i%2 == 1 )); then
+		out="$out $argument";
+	fi;
+	(( i++ ));
+done;
+echo $out
 
+# PPA 2
+total=0;
+for hash in `cat result`; do
+	filename=`grep "$hash" map | cut -d " " -f 2`;
+	amt=`cat $filename | head -n 1 | cut -d "$" -f 2`;
+	total=$(($total+$amt));
+done;
+echo $total;
