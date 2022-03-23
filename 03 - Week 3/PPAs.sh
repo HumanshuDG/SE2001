f085c3f5bec944b3935f67cb9e73d1acd9f532d1
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2022-03-23 17:50:55 +0530

    Update PPAs.sh

--- 03 - Week 3/PPAs.sh
+++ 03 - Week 3/PPAs.sh
@@ -1,1 +1,10 @@
+# PPA 1
+ls -l *.txt > textFiles.txt 2> noFiles.txt && echo found
 
+# PPA 2
+cal $month 2022 > $month.txt 2> error.txt
+
+# PPA 3
+test $2 2> errorlog;
+test -e 2>> errorlog;
+test -n 2>> errorlog;
