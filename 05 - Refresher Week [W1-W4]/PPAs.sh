7dd2f1c16b775e62ab5c92a93f1b4a60d40b56f7
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2022-03-23 17:56:08 +0530

    Update PPAs.sh

--- 05 - Refresher Week [W1-W4]/PPAs.sh
+++ 05 - Refresher Week [W1-W4]/PPAs.sh
@@ -2,21 +2,13 @@
 echo `uname -a` | cut -d ' '  -f 13
 
 # PPA 2
-script() {
-    grep 'FAILED LOGIN' myauth.log | wc -l
-}
+grep 'FAILED LOGIN' myauth.log | wc -l
 
 # PPA 3
-script() {
-    grep 'New session' myauth.log | cut -d " " -f 11 | cut -d "." -f 1 | sort --unique
-}
+grep 'New session' myauth.log | cut -d " " -f 11 | cut -d "." -f 1 | sort --unique
 
 # PPA 4
-script() {
-    grep 'session opened for user guest' myauth.log | cut -d " " -f 1,2,3 | tail -n 1
-}
+grep 'session opened for user guest' myauth.log | cut -d " " -f 1,2,3 | tail -n 1
 
 # PPA 5
-script() {
-    grep 'su:' -b myauth.log | grep 'session opened' | grep 'by student' | cut -d " " -f 11
-}
+grep 'su:' -b myauth.log | grep 'session opened' | grep 'by student' | cut -d " " -f 11
